5979fe6f4b7c4a5825b852cf7478b4c7d298737f
Peter Boraros <pborky@pborky.sk>
2014-11-12 13:45:10 +0100

    optimize imports

--- payment_dues_monthly.py
+++ payment_dues_monthly.py
@@ -2,13 +2,7 @@ import os; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
 from project.roster.transactions import payment_due
 from project.roster.models import Buddy
-from project import settings
-from fiobank import FioBank
-from django.core.exceptions import ObjectDoesNotExist
 
-from decimal import Decimal
-
-from time import sleep
 import datetime
 import logging
 
