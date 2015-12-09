9207ac377ec920a3bb09c9fee57994b75efdfb6d
Peter Boráros <pborky@pborky.sk>
2015-12-09 19:42:16 +0100

    opraveny export kariet

--- export_cards.py
+++ export_cards.py
@@ -2,17 +2,12 @@
 #!/usr/bin/env python
 
 import os; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brmburo.settings')
-
-from brmburo.transactions import payment_due, account_sum
-from brmburo.models import Buddy, LogicTransactionSplit
-
-import datetime
 import logging
-
 logger = logging.getLogger(__name__)
-
 from django.utils.timezone import now
 from brmburo.models import *
+
+
 n = now()
 for sp in SecurityPrincipal.objects.filter(since__lt=n,until__gt=n,buddy__type__symbol='member'):
         print sp.buddy.nickname, sp.value
