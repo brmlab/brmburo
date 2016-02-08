28cc51d96ba24cca653483b8b64ae7e7f75a355a
Peter Boráros <pborky@pborky.sk>
2016-02-08 21:21:46 +0100

    dont fail if buddy account does not exist

--- account_balances.py
+++ account_balances.py
@@ -9,8 +9,11 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-for buddy in Buddy.objects.filter(type__symbol='member'):
+for buddy in Buddy.objects.all().order_by('nickname'):
+    if buddy.logic_account is None:
+        continue
     template = u'Balance for @%(nickname)s is %(balance)s %(curr)s.'
+    #template = u'@%(nickname)s\t%(balance)s\t%(curr)s'
     logger.info(template % dict(
         curr = buddy.logic_account.currency.symbol,
         nickname = buddy.nickname,
