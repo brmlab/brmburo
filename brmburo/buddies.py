272eb56f49c3c0f3803994498803eb249dd5f212
Peter Boráros <pborky@pborky.sk>
2016-05-28 03:46:43 +0200

    * added handling of unauthorized exceptions * added pagination helpers * moved prime generation

--- brmburo/buddies.py
+++ brmburo/buddies.py
@@ -1,23 +1,27 @@
-from decimal import Decimal
 import logging
+import random
 
 from django.utils.timezone import now
-from django.db.models import Q
+from random import shuffle
 
-from datetime import timedelta
-from models import LogicAccount, LogicAccountType, LogicTransaction, LogicTransactionSplit, Buddy, BuddyEvent, Currency
+from models import LogicAccount, LogicAccountType, Buddy, BuddyEvent, BuddyEventType, Currency
 
 
 logger = logging.getLogger(__name__)
 
 
-def buddy_after_create(buddy):
+def buddy_for_logic_account(account):
+    buddy = Buddy.objects.filter(logic_account=account)
+    return buddy[0] if buddy.exists() else None
+
+
+def after_create_buddy(buddy):
     if buddy.type.symbol == 'member':
         buddy.logic_account = LogicAccount.objects.create(
-            name = "Payments from %s %s" % (buddy.first_name, buddy.surname),
-            symbol = "@%s" % buddy.nickname,
-            currency = Currency.objects.get(symbol="CZK"),
-            type = LogicAccountType.objects.get(symbol = "credit")
+            name="Payments from %s %s" % (buddy.first_name, buddy.surname),
+            symbol="@%s" % buddy.nickname,
+            currency=Currency.objects.get(symbol="CZK"),
+            type=LogicAccountType.objects.get(symbol="credit")
         )
     buddy.save()
     if buddy.type.symbol == 'member':
@@ -29,3 +33,42 @@ def buddy_after_create(buddy):
         )
     event.save()
 
+
+def rabin_miller(num):
+    """
+    :param num: number to test
+    :return: Returns if number is a prime according to Rabin-Miller primality test
+    """
+
+    s = num - 1
+    t = 0
+    while s % 2 == 0:
+        # keep halving s while it is even (and use t
+        # to count how many times we halve s)
+        s = s // 2
+        t += 1
+
+    for trials in range(5): # try to falsify num's primality 5 times
+        a = random.randrange(2, num - 1)
+        v = pow(a, s, num)
+        if v != 1: # this test does not apply if v is 1.
+            i = 0
+            while v != (num - 1):
+                if i == t - 1:
+                    return False
+                else:
+                    i += 1
+                    v = (v ** 2) % num
+    return True
+
+def get_default_prime():
+    #select uid in range [1111,9999] that is not already taken
+    buddy_uids = set(Buddy.objects.values_list('uid', flat=True))
+    candidates = range(1111, 9999, 2)
+    shuffle(candidates)
+
+    for prime_candidate in candidates:
+        if prime_candidate in buddy_uids:
+            continue
+        if rabin_miller(prime_candidate):
+            return prime_candidate
--- brmburo/helpers.py
+++ brmburo/helpers.py
@@ -1,7 +1,8 @@
-__author__ = 'pborky'
+from functools import wraps
+from django.core.paginator import Page, Paginator, PageNotAnInteger, EmptyPage
+from django.utils.decorators import available_attrs
 
 import types
-import random
 from django.contrib import messages
 from django.views.decorators import http
 from django.views.decorators.http import require_GET, require_POST
@@ -13,6 +14,10 @@ from django.contrib.admin.sites import AlreadyRegistered
 
 from functional import partial
 
+try:
+    from .settings import ITEMS_PER_PAGE
+except ImportError:
+    ITEMS_PER_PAGE = 15
 
 _HTTP_METHODS = ('get','post', 'option', 'put', 'delete', 'head')
 
@@ -24,7 +29,6 @@ def autoregister(*app_list):
                 admin.site.register(model)
             except AlreadyRegistered:
                 pass
-
 
 
 class combine(dict):
@@ -39,6 +43,7 @@ class combine(dict):
     def __len__(self):
         return sum(len(d) for d in self.dicts)
 
+
 class Decorator(object):
     def __init__(self, func):
         self.func = func
@@ -46,7 +51,11 @@ class Decorator(object):
     def __call__(self, *args, **kwargs):
         return self.func.__call__(*args, **kwargs)
 
-def view(pattern, template = None, form_cls = None, redirect_to = None, redirect_attr = None, invalid_form_msg = 'Form is not valid.', decorators = (), **kwargs):
+
+class NotAuthorizedException(Exception):
+    pass
+
+def view(pattern, template = None, template_401='401.html', form_cls = None, redirect_to = None, redirect_attr = None, invalid_form_msg = 'Form is not valid.', decorators = (), **kwargs):
     class Wrapper(Decorator):
         def __init__(self, obj):
             super(Wrapper, self).__init__(obj)
@@ -146,8 +155,12 @@ def view(pattern, template = None, form_cls = None, redirect_to = None, redirect
             except AttributeError as e:
                 raise e
                 #return HttpResponseServerError()
+            except NotAuthorizedException:
+                context_vars = { }
+                context_vars.update(kwargs)
+                return render(request, template_401, context_vars)
 
-            redirect_addr = combine(request.GET, request.POST) .get(redirect_attr, redirect_to)
+            redirect_addr = combine(request.GET, request.POST).get(redirect_attr, redirect_to)
 
             if redirect_addr:
                 context_vars = { }
@@ -171,40 +184,133 @@ def view(pattern, template = None, form_cls = None, redirect_to = None, redirect
     return Wrapper
 
 
-
-
 def view_GET(*args, **kwargs):
     decorators = kwargs.pop('decorators', ())
     return view(*args, decorators=(require_GET,)+decorators , **kwargs)
+
 
 def view_POST(*args, **kwargs):
     decorators = kwargs.pop('decorators', ())
     return view(*args, decorators=(require_POST,)+decorators , **kwargs)
 
-def rabin_miller(num):
+
+def superuser_required(function):
     """
-    :param num: number to test
-    :return: Returns if number is a prime according to Rabin-Miller primality test
+    Check if superuser is present, otherwise raise NotAuthorizedException.
     """
+    @wraps(function, assigned=available_attrs(function))
+    def inner(request, *a, **kw):
+        if request.user.is_superuser:
+            return function(request, *a, **kw)
+        raise NotAuthorizedException('Superuser is required.')
 
-    s = num - 1
-    t = 0
-    while s % 2 == 0:
-        # keep halving s while it is even (and use t
-        # to count how many times we halve s)
-        s = s // 2
-        t += 1
+    if callable(function):
+        return inner
 
-    for trials in range(5): # try to falsify num's primality 5 times
-        a = random.randrange(2, num - 1)
-        v = pow(a, s, num)
-        if v != 1: # this test does not apply if v is 1.
-            i = 0
-            while v != (num - 1):
-                if i == t - 1:
-                    return False
-                else:
-                    i = i + 1
-                    v = (v ** 2) % num
-    return True
+    raise ValueError('Not a function')
 
+
+def staff_required(function):
+    """
+    Check if superuser is present, otherwise raise NotAuthorizedException.
+    """
+    @wraps(function, assigned=available_attrs(function))
+    def inner(request, *a, **kw):
+        if request.user.is_staff:
+            return function(request, *a, **kw)
+        raise NotAuthorizedException('Superuser is required.')
+
+    if callable(function):
+        return inner
+
+    raise ValueError('Not a function')
+
+
+class PageWrapper(object):
+    """
+    Wrap the page object of Page class and call callback for each item.
+    Operates lazily unless __getitem__ is called.
+    """
+    def __init__(self, page, callback, lazy=True):
+        if not callable(callback):
+            raise ValueError('Not a function')
+        if not isinstance(page, Page):
+            raise ValueError('Not a Page')
+        self._callback = callback
+        self.page = page
+
+    @property
+    def number(self):
+        return self.page.number
+
+    @property
+    def paginator(self):
+        return self.page.paginator
+
+    def __repr__(self):
+        return repr(self.page)
+
+    def __len__(self):
+        return len(self.page)
+
+    def __getitem__(self, index):
+        return self._callback(self.page[index])
+
+    def __iter__(self):
+        return (self._callback(i) for i in self.page)
+
+    def __contains__(self, value):
+        return self.page.__contains__(value)
+
+    def index(self, value):
+        return self.page.index(value)
+
+    def count(self, value):
+        return self.page.count(value)
+
+    def has_next(self):
+        return self.page.has_next()
+
+    def has_previous(self):
+        return self.page.has_previous()
+
+    def has_other_pages(self):
+        return self.page.has_other_pages()
+
+    def next_page_number(self):
+        return self.page.next_page_number()
+
+    def previous_page_number(self):
+        return self.page.previous_page_number()
+
+    def start_index(self):
+        return self.page.start_index()
+
+    def end_index(self):
+        return self.page.end_index()
+
+
+class LazyPageWrapper(PageWrapper):
+
+    def __init__(self, page, callback):
+        super(LazyPageWrapper, self).__init__(page, callback)
+
+    def __getitem__(self, index):
+        raise KeyError('Invalid for lazy operation')
+
+
+def paginate(results, number=None, callback=None):
+    paginator = Paginator(results, ITEMS_PER_PAGE)
+    try:
+        page = paginator.page(number)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        page = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        page = paginator.page(paginator.num_pages)
+
+    if callable(callback):
+        return LazyPageWrapper(page, callback)
+
+    return page
